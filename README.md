0c8f17ad402d568d49512cac1f26670a8ec9d4d4
Jake Head <jake4head@outlook.com>
2024-12-20 12:34:03 -0330

    Conan package manager build working

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Pum is my first attempt at create a compiler which compiles to binary.
 
 - platform: Linux WSL2
 - cmake 3.22.1
+- conan 2.11.0
--- build.sh
+++ build.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+./build_deps.sh
+
+original_dir=$(pwd)
+
+return_to_original_dir() {
+    cd "$original_dir" || exit
+}
+
+trap return_to_original_dir EXIT
+
 mkdir -p build
-cmake -B ./build -S ./src
-cmake --build ./build
+
+cd build
+
+cmake .. -DCMAKE_TOOLCHAIN_FILE=conan_toolchain.cmake -DCMAKE_BUILD_TYPE=Release
+cmake --build .
